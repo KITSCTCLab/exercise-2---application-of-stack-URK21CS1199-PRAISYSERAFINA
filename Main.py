1504fa39c17a3c9930e62636377583d96804e5ba
URK21CS1199-PRAISYSERAFINA <109951858+URK21CS1199-PRAISYSERAFINA@users.noreply.github.com>
2022-07-29 20:52:55 +0530

    Update Main.py
    3rd commit

--- Main.py
+++ Main.py
@@ -28,7 +28,7 @@ class Evaluate:
     # The data which is popped out if the stack is not empty.
     if not self.isEmpty():
       self.top -= 1
-      return self.stack.pop(1)
+      return self.stack.pop(-1)
     
 
 
